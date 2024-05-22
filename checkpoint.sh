f3d5f0549e0a178a837ffcb6acdb912f7d118838
Jeff Fang <jeff.fang@airwallex.com>
2024-05-22 08:26:43 +0800

    initial commit

--- checkpoint.sh
+++ checkpoint.sh
@@ -10,7 +10,7 @@ echo "Using CRaC enabled JDK $url"
 
 ./gradlew clean assemble
 docker build -t jeffawx/crac-test:builder --build-arg CRAC_JDK_URL=$url .
-docker run -d --privileged --rm --name=crac-test --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/build/libs:/opt/mnt -e FLAG=$1 jeffawx/crac-test:builder
+docker run -d --privileged --rm --name=crac-test --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/build/libs:/opt/mnt -e FLAG=$1 -e APP_SECRET=test jeffawx/crac-test:builder
 echo "Please wait during creating the checkpoint..."
 sleep 10
 docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=crac-test") jeffawx/crac-test:checkpoint
--- restore.sh
+++ restore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-docker run --cap-add CHECKPOINT_RESTORE --cap-add SYS_ADMIN --rm -p 8080:8080 --name crac-test -e APP_SECRET=abcde jeffawx/crac-test:checkpoint
+docker run --cap-add CHECKPOINT_RESTORE --cap-add SYS_ADMIN --rm -p 8080:8080 -e APP_SECRET=secret --name crac-test jeffawx/crac-test:checkpoint
